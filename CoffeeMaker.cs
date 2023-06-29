a87556542c880c72ff5e5fec463797c46787eda0
Stu <stucoding@protonmail.com>
2023-06-29 11:30:42 +0200

    Add files via upload

---
+++ CoffeeMaker.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+class CofffeeMachine
+{
+    private int waterLevel;
+    private int coffeeBeansLevel;
+    private int milkLevel;
+
+    public CofffeeMachine()
+    {
+        waterLevel = 0;
+        coffeeBeansLevel = 0;
+        milkLevel = 0;
+    }
+
+    public void AddWater(int amount)
+    {
+        waterLevel += amount;
+        Console.WriteLine($"Added {amount} ml of water. Current water level: {waterLevel} ml");
+    }
+
+    public void AddCoffeeBeans(int amount)
+    {
+        coffeeBeansLevel += amount;
+        Console.WriteLine($"Added {amount} g of coffee beans. Current coffee bean level: {coffeeBeansLevel} g");
+    }
+
+    public void AddMilk(int amount)
+    {
+        milkLevel += amount;
+        Console.WriteLine($"Added {amount} ml of milk. Current milk level: {milkLevel} ml");
+    }
+
+    public void MakeEspresso()
+    {
+        if (waterLevel >= 30 && coffeeBeansLevel >= 10)
+        {
+            waterLevel -= 30;
+            coffeeBeansLevel -= 10;
+            Console.WriteLine("Espresso require 30ml water and 10g coffee beans! Enter to continue.");
+            Console.ReadLine();
+            Console.WriteLine("Making Espresso...");
+            Thread.Sleep(10000);
+            Console.WriteLine("Enjoy your drink!");
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("Not enough water or coffee beans. Please refill.");
+        }
+    }
+
+    public void MakeCappucino()
+    {
+        if (waterLevel >= 60 && coffeeBeansLevel >= 20 && milkLevel >= 50)
+        {
+            waterLevel -= 60;
+            coffeeBeansLevel -= 20;
+            milkLevel -= 50;
+            Console.WriteLine("Cappucino require 60ml water and 20g coffee beans! Enter to continue.");
+            Console.ReadLine();
+            Console.WriteLine("Making Cappuccino...");
+            Thread.Sleep(10000);
+            Console.WriteLine("Enjoy your drink!");
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("Not enough water, coffee beans, or milk. Please refill.");
+        }
+    }
+
+    public void CheckLevels()
+    {
+        Console.WriteLine($"Water level: {waterLevel} ml");
+        Console.WriteLine($"Coffee beans level: {coffeeBeansLevel} g");
+        Console.WriteLine($"Milk level: {milkLevel} ml");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml.Serialization;
+
+class Program
+{
+    static void Main()
+    {
+        CofffeeMachine machine = new CofffeeMachine();
+        
+        machine.AddWater(200);
+        
+        machine.AddCoffeeBeans(50);
+        
+        machine.AddMilk(100);
+        
+        machine.CheckLevels();
+
+        Console.WriteLine("");
+        Console.WriteLine("Espresso: 1 - Cappucino: 2");
+        int choice = Convert.ToInt32(Console.ReadLine());
+
+        if (choice == 1)
+        {
+            machine.MakeEspresso();
+        }
+        else
+        {
+            machine.MakeCappucino();
+        }
+    }
+}
